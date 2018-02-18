3c7593ff636cda41600de2dc66c8f22f3c2b400a
gbooth27 <gbooth@middlebury.edu>
2018-02-18 13:19:07 -0500

    formatting

--- machine_learning/nnet.py
+++ machine_learning/nnet.py
@@ -1,4 +1,4 @@
-from keras.models import Sequential
+from keras.models import Model
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
@@ -8,3 +8,12 @@ from keras.models import load_model
 import numpy as np
 import argparse
 from matplotlib import pyplot as plt
+
+# https://keras.io/layers/recurrent/
+
+def load_net(n):
+    """
+    Creates a neural network of the correct size for optimization
+    :param n: number of particles
+    :return:
+    """
