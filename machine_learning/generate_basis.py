2cbf75888894015a94198488a5937cb31a97da46
gbooth27 <gbooth@middlebury.edu>
2018-02-19 16:50:02 -0500

    adding code for generating training set for Psi

--- machine_learning/generate_basis.py
+++ machine_learning/generate_basis.py
@@ -1,7 +1,45 @@
-def generate(n):
-    """
-    Generates a vector of all 2^n possible spin basis states of the system
-    :param n: number of particles
-    :return:
-    """
-    return None
+import numpy as np
+
+
+class Psi(object):
+
+    def __init__(self, n):
+        self.size = n
+        self.basis = self.generate()
+
+    def recusive_gen(self, state, index, S):
+        """
+        recursively generates all possible combos of state basis vectors
+        :param state: initial state (all zeros)
+        :param index: initial index to change
+        :return:
+        """
+        # base case
+        if index >= len(state):
+            return S
+        else:
+            S.append(state)
+            self.recusive_gen(list(state), index+1, S)
+            state[index] = 1
+            #S.append(state)
+            self.recusive_gen(list(state), index + 1, S)
+            return S
+
+
+
+    def generate(self):
+        """
+        Generates a vector of all 2^n possible spin basis states of the system
+        :param n: number of particles
+        :return:
+        """
+        S = []
+        s_initial = [0 for _ in range(self.size)]
+        S = self.recusive_gen(s_initial, 0, S)
+        arr = np.array(S)
+        return arr
+
+if __name__ == '__main__':
+    p = Psi(4)
+    print(p.basis)
+
--- machine_learning/nnet.py
+++ machine_learning/nnet.py
@@ -1,4 +1,4 @@
-from keras.models import Model
+from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
@@ -17,3 +17,46 @@ def load_net(n):
     :param n: number of particles
     :return:
     """
+    return None
+
+def run_nnet(x, y, gpu, m):
+    """
+    Run neural net for power predictions.
+    :param x: features for training
+    :param y: labels for data
+    :param gpu:use gpu optimization
+    :return: model
+    """
+    if m != "":
+        # load the model so that we can continue training
+        model = load_model(m)
+    else:
+        # Create model.
+        model = Sequential()
+        dim1 = len(x)
+        dim2 = len(x[0])
+        # Add the layers.
+        # Tuning
+        model.add(Dense(dim1, input_dim=dim2, kernel_initializer='random_uniform', activation='relu'))
+        model.add(Dense(400, kernel_initializer='random_uniform', activation='relu'))
+        model.add(Dense(200, kernel_initializer='random_uniform', activation='relu'))
+        model.add(Dense(1, kernel_initializer='random_uniform'))
+
+
+        # Set the optimizer.
+        #sgd = optimizers.SGD(lr=0.01, clipnorm=2.)#, momentum=0.1, nesterov=True)
+        #sgd = optimizers.Adagrad(clipnorm=2.)
+        sgd = optimizers.Adam()
+        #sgd = optimizers.Adadelta(clipnorm=2.)
+        # Compile model.
+        model.compile(loss='mse', optimizer=sgd, metrics=["mae", percent_err])
+    if gpu:
+        # Fit the model.
+        # DO NOT CHANGE GPU BATCH SIZE, CAN CAUSE MEMORY ISSUES
+        model.fit(x, y, epochs=50, batch_size=512, verbose=2 , validation_split=0.2)
+    else:
+        # Fit the model.
+        # Feel free to change this batch size.
+        model.fit(x, y, epochs=100, batch_size=4096, verbose =2, validation_split=0.2)
+    return model
+
