e1b5d6a64c525f3e2fe8110ea7cba520d8aa6877
gbooth27 <gbooth@middlebury.edu>
2018-02-19 18:19:21 -0500

    adding code for generating training set for Psi

--- machine_learning/generate_basis.py
+++ machine_learning/generate_basis.py
@@ -6,6 +6,7 @@ class Psi(object):
     def __init__(self, n):
         self.size = n
         self.basis = self.generate()
+        self.weights = self.gen_weights()
 
     def recusive_gen(self, state, index, S):
         """
@@ -18,26 +19,34 @@ class Psi(object):
         if index >= len(state):
             return S
         else:
+            # append the current state
             S.append(state)
+            # recursively generate without bit flip
             self.recusive_gen(list(state), index+1, S)
+            # flip bit then recursively generate
             state[index] = 1
-            #S.append(state)
             self.recusive_gen(list(state), index + 1, S)
             return S
-
-
 
     def generate(self):
         """
         Generates a vector of all 2^n possible spin basis states of the system
-        :param n: number of particles
-        :return:
+        :return: numpy array of various states
         """
         S = []
         s_initial = [0 for _ in range(self.size)]
         S = self.recusive_gen(s_initial, 0, S)
+        S = sorted(S)
         arr = np.array(S)
         return arr
+
+    def gen_weights(self):
+        """
+        generates the vector of weights
+        :return: psi representing weights
+        """
+        psi = [1 for _ in range(2**self.size)]
+        return np.array(psi)
 
 if __name__ == '__main__':
     p = Psi(4)
