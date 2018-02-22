6e8b611ac0d2f30a261c513dd8128faa2d1a3a25
gbooth27 <gbooth@middlebury.edu>
2018-02-22 00:57:20 -0500

    trying to get the shapes in the network to function properly

--- machine_learning/nnet.py
+++ machine_learning/nnet.py
@@ -3,10 +3,12 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras import optimizers
+import keras
 from keras.models import load_model
 import machine_learning.generate_basis as wave
 import tensorflow as tf
 import scipy as sp
+from keras import backend as K
 
 
 
@@ -14,8 +16,18 @@ import numpy as np
 import argparse
 from matplotlib import pyplot as plt
 
+import keras.callbacks as cbks
+class CustomMetrics(cbks.Callback):
+
+    def on_epoch_end(self, epoch, logs=None):
+        for k in logs:
+            if k.endswith('energy'):
+               print (logs[k])
+
+
 # https://keras.io/layers/recurrent/
-psi = wave.Psi(6, 1)
+DIM = 10
+psi = wave.Psi(DIM, 1)
 
 def energy(y_true, y_pred):
     """
@@ -23,12 +35,19 @@ def energy(y_true, y_pred):
     :param y_pred:
     :return:
     """
-    sess = tf.InteractiveSession()
-    y= y_true.eval()
-    psi.weights[y_true] = y_pred
-    un_norm = np.dot(np.dot(psi.weights.T, psi.Hamiltonian.toarray()), psi.weights)[0][0]
-    norm = un_norm/np.dot(psi.weights.T, psi.weights)[0][0]
-    return norm
+    #sess = tf.InteractiveSession()
+    #y= K.sum(y_true, 1)
+    ham = K.variable(psi.Hamiltonian.toarray())
+    #print(K.shape(ham))
+    s = K.shape(y_pred)
+
+    y_pred= K.reshape(y_pred, (s[1], -1))
+    #s = K.shape(y_pred)
+    un_norm = K.dot(K.dot(y_pred, ham), y_pred)
+    #psi.weights[y_true] = y_pred
+    #un_norm = np.dot(np.dot(psi.weights.T, psi.Hamiltonian.toarray()), psi.weights)[0][0]
+    #norm = un_norm/np.dot(psi.weights.T, psi.weights)[0][0]
+    return un_norm
 
 def min_energy(p):
     un_norm = np.dot(np.dot(p.T, psi.Hamiltonian.toarray()), p)
@@ -52,7 +71,7 @@ def run_nnet(x, gpu, m):
     :param gpu:use gpu optimization
     :return: model
     """
-    y = [i for i in range(len(psi.weights))]
+    y = psi.weights #[i for i in range(len(psi.weights))]
     if m != "":
         # load the model so that we can continue training
         model = load_model(m)
@@ -63,10 +82,12 @@ def run_nnet(x, gpu, m):
         dim2 = len(x[0])
         # Add the layers.
         # Tuning
-        model.add(Dense(dim1, input_dim=dim2, kernel_initializer='random_uniform', activation='relu'))
+        model.add(Dense(dim1, input_dim = dim2, kernel_initializer='random_uniform', activation='relu'))
         #model.add(Dense(400, kernel_initializer='random_uniform', activation='relu'))
-        model.add(Dense(200, kernel_initializer='random_uniform', activation='relu'))
-        model.add(Dense(1, kernel_initializer='random_uniform'))
+        #model.add(Dense(200, kernel_initializer='random_uniform', activation='relu'))
+        model.add(Dense(2**DIM, kernel_initializer='random_uniform', activation="relu"))#output_dim = (dim1,dim2)))
+        #model.add(keras.layers.Reshape())
+        #model.add(keras.layers.Reshape(dim1, 1))
 
 
         # Set the optimizer.
@@ -76,6 +97,7 @@ def run_nnet(x, gpu, m):
         #sgd = optimizers.Adadelta(clipnorm=2.)
         # Compile model.
         model.compile(loss=energy, optimizer=sgd)
+        print(model.summary())
     if gpu:
         # Fit the model.
         # DO NOT CHANGE GPU BATCH SIZE, CAN CAUSE MEMORY ISSUES
@@ -83,10 +105,10 @@ def run_nnet(x, gpu, m):
     else:
         # Fit the model.
         # Feel free to change this batch size.
-        model.fit(x, y, epochs=100, batch_size=4096, verbose =2, validation_split=0.2)
+        model.fit(x, y, epochs=100, batch_size=4096, verbose =2, validation_split=0.2, callbacks=[CustomMetrics()])
     return model
 
 if __name__ == '__main__':
-    #run_nnet(psi.basis, True, "")
-    min = sp.optimize.minimize(min_energy, psi.weights)
-    print(min)
+    run_nnet(psi.basis, True, "")
+    #min = sp.optimize.minimize(min_energy, psi.weights)
+    #print(min)
