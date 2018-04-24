560ff569d9da36f5889106cfe04f4005831d63a9
grahambooth <gbooth@middlebury.edu>
2018-04-24 14:15:44 -0400

    restructure

---
+++ thesis/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='thesis',
+      version='1.0',
+      description='RBM stuff',
+      author='Graham Booth',
+      author_email='gbooth@middlebury.edu',
+     )
